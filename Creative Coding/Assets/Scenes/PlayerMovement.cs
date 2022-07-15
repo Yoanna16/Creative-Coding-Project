5e56484f0b2dabc5270b4205401a357f6fd99c0c
Yoanna16 <yoannaborisova@gmail.com>
2022-07-15 18:48:56 +0200

    Add shooting and rotation to Player controller

--- Creative Coding/Assets/Scenes/PlayerMovement.cs
+++ Creative Coding/Assets/Scenes/PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
 
     Vector2 movement; 
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
@@ -16,12 +18,18 @@ public class PlayerMovement : MonoBehaviour
         //input
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+
+       mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
         //movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle; 
     }
 }
 
